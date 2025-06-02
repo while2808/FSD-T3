02cc5961ee148367136d9ebe98e78980826396e8
Romil <goyaniyash120@gmail.com>
2025-06-02 12:41:30 +0530

    Add files via upload

---
+++ Ch-7 React/Prop_14.js
@@ -0,0 +1,14 @@
+function P3(props){
+    return(
+        <div>
+            <ul>
+                <li>{props.name}</li>
+                <li>{props.unin}</li>
+                <li>{props.age}</li>
+                <li>{props.spi}</li>
+            </ul>
+        </div>
+    )
+}
+
+export default P3
