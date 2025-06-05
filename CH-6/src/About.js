91497cef97347a9c2990943de197f63b6d55714a
Romil <goyaniyash120@gmail.com>
2025-06-05 12:25:09 +0530

    Add files via upload

---
+++ CH-6/src/About.js
@@ -0,0 +1,13 @@
+function About() {
+    return (
+      <div>
+        <h1>About</h1>
+        <ul>
+          <li>Manager 1</li>
+          <li>Manager 2</li>
+          <li>Manager 3</li>
+        </ul>
+      </div>
+    );
+}
+export default About
